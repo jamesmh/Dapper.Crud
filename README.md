71d1794ae1527526c3968fc90948a66861057cdf
James Hickey <jamesmichaelhickey@gmail.com>
2018-04-10 16:19:01 -0300

    moved src to library

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dapper.Crud
 
 Extensions methods for `IDbConnection` to enable easy CRUD operations using Dapper.
+
+Work in progress ;)
---
+++ Tests/TestInsert.cs
@@ -0,0 +1,13 @@
+
+namespace Tests
+{
+    [TestClass]
+    public class TestInsert
+    {
+        [TestMethod]
+        public void InsertSingle()
+        {
+            var mock = new Mock<SqlConnection>();
+        }
+    }
+}
